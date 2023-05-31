a77062bdb6641f0ecbd63775a446d0ee320eee75
Brett van Beynum <maildrop444@gmail.com>
2023-05-30 20:46:10 -0400

    Fix test with new folder structure

--- client/tests/index.test.js
+++ client/tests/index.test.js
@@ -5,7 +5,7 @@
 import React from "react";
 import { render, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import Index from "../src/index.jsx";
+import Index from "../public/index.jsx";
 
 describe("Index component", () => {
 	
