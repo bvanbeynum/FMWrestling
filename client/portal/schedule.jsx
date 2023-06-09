1999af41fcefc88c599515cc20c3eba69f3d869c
Brett van Beynum <maildrop444@gmail.com>
2023-06-09 13:34:48 -0400

    Formatting changes

--- client/portal/schedule.jsx
+++ client/portal/schedule.jsx
@@ -241,15 +241,15 @@ const Schedule = (props) => {
 			:
 			
 			<div className="row">
-				<button aria-label="Add" className="icon" onClick={ () => setEditItem("newEvent") }>
+				<div className="rowContent">
+					<h3>New Event</h3>
+				</div>
+				
+				<button aria-label="Add" className="action" onClick={ () => setEditItem("newEvent") }>
 					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
 						<path d="M440-200v-240H200v-80h240v-240h80v240h240v80H520v240h-80Z"/>
 					</svg>
 				</button>
-
-				<div className="rowContent">
-					<h3>New Event</h3>
-				</div>
 			</div>
 
 			}
@@ -307,20 +307,20 @@ const Schedule = (props) => {
 			:
 			
 			<div key={ event.id } data-testid={ event.id } className="row">
-				<button aria-label="Edit" className="icon" onClick={ () => setEditItem(event.id) }>
+				<div className="rowContent">
+					<h3>{ event.name }</h3>
+
+					<div className="subHeading">
+						<div>Date: { event.date.toLocaleDateString() }</div>
+						<div>Location: { event.location }</div>
+					</div>
+				</div>
+				
+				<button aria-label="Edit" className="action" onClick={ () => setEditItem(event.id) }>
 					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
 						<path d="M200-200h56l345-345-56-56-345 345v56Zm572-403L602-771l56-56q23-23 56.5-23t56.5 23l56 56q23 23 24 55.5T829-660l-57 57Zm-58 59L290-120H120v-170l424-424 170 170Zm-141-29-28-28 56 56-28-28Z"/>
 					</svg>
 				</button>
-
-				<div className="rowContent">
-					<h3>{ event.name }</h3>
-
-					<div className="row spread subHeading">
-						<div>{ event.location }</div>
-						<div>{ event.date.toLocaleDateString() }</div>
-					</div>
-				</div>
 			</div>
 			}
 		</div>
