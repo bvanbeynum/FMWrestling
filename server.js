29fd1ed8f7762da1dd6d08fa7ae81548c13613e5
Brett van Beynum <maildrop444@gmail.com>
2023-09-01 09:02:08 -0400

    Get right current directory

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ import express from "express";
 // Declarations =======================================================================
 
 const port = config.port || 9201;
-// const currentDirectory = path.resolve(process.cwd());
-const currentDirectory = dirname(currentFile);
+const currentDirectory = path.join(path.resolve(process.cwd()), "/web");
+// const currentDirectory = dirname(currentFile);
 
 mongoose.connect(`mongodb://${config.db.user}:${config.db.pass}@${config.db.servers.join(",")}/${config.db.db}?authSource=${config.db.authDB}`, {useNewUrlParser: true, useUnifiedTopology: true });
 
