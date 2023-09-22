4c590d3747b88acb1fa9aec0cae3ee0c13bf5e02
Brett van Beynum <maildrop444@gmail.com>
2023-09-22 16:23:09 -0400

    Updated tests

--- client/portal/tests/teamview.test.js
+++ client/portal/tests/teamview.test.js
@@ -50,56 +50,7 @@ describe("Flo Event Component", () => {
 
 		await waitFor(() => expect(global.fetch).toHaveBeenCalledWith(`/api/teamviewload?id=${ team.id }`));
 		await waitFor(() => expect(screen.getByText(team.name)).toBeInTheDocument());
-		(await waitFor(() => expect(screen.getByTestId(team.wrestlers[0].id)))).toBeInTheDocument();
 		
-	});
-
-	it("adds a new wrestler", async () => {
-
-		const newWrestler = {
-			id: "savewrestler",
-			firstName: "Test",
-			lastName: "Wrestler",
-			division: "Varsity",
-			weightClass: "111"
-		}
-		
-		await waitFor(() => {
-			render(<TeamViewComponent />);
-		});
-
-		await waitFor(() => expect(global.fetch).toHaveBeenCalledWith(`/api/teamviewload?id=${ team.id }`));
-		await waitFor(() => expect(screen.getByText(team.name)).toBeInTheDocument());
-		
-		global.fetch = jest.fn().mockResolvedValue({
-			ok: true,
-			status: 200,
-			json: jest.fn().mockResolvedValue({
-				wrestler: newWrestler
-			})
-		});
-
-		const addButton = await screen.findByRole("button", { name: /^add wrestler$/i });
-		fireEvent.click(addButton);
-
-		const firstNameInput = await screen.findByLabelText(/^first name$/i),
-			lastNameInput = await screen.findByLabelText(/^last name$/i),
-			divisionInput = await screen.findByLabelText(/^wrestler division$/i),
-			weightClassInput = await screen.findByLabelText(/^wrestler weight class$/i),
-			saveButton = await screen.findByRole("button", { name: /^save$/i });
-
-		fireEvent.change(firstNameInput, { target: { value: newWrestler.firstName }});
-		fireEvent.change(lastNameInput, { target: { value: newWrestler.lastName }});
-		fireEvent.change(divisionInput, { target: { value: newWrestler.division }});
-		fireEvent.change(weightClassInput, { target: { value: newWrestler.weightClass }});
-
-		fireEvent.click(saveButton);
-
-		await waitFor(() => expect(global.fetch).toHaveBeenCalledWith(`/api/teamswrestlersave?teamid=${ team.id }`, expect.objectContaining({
-			body: expect.stringContaining(newWrestler.firstName)
-		})));
-		expect(await screen.findByTestId(newWrestler.id)).toBeInTheDocument();
-
 	});
 
 });
---
+++ client/portal/tests/teamwrestlers.test.js
@@ -0,0 +1,59 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from "react";
+import { fireEvent, render, screen, waitFor, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import TeamWrestlers from "../teamwrestlers.jsx";
+
+describe("Team Wrestler Component", () => {
+
+	const wrestlers = [{ id: "wrestler1", firstName: "Test", lastName: "Wrestler", division: "Varsity", weightClass: "111", position: 1 }];
+
+	const updateWrestlers = wrestlers => {};
+	const addWrestler = wrestler => {};
+
+	it("Initializes the component", async () => {
+
+		render(<TeamWrestlers wrestlers={ wrestlers } updateWrestlers={ updateWrestlers } addWrestler={ addWrestler } savingError={ "" } />)
+		expect(screen.getByTestId(wrestlers[0].id)).toBeInTheDocument();
+
+	});
+
+	it("adds a new wrestler", async () => {
+
+		const newWrestler = {
+			id: "savewrestler",
+			firstName: "Test",
+			lastName: "Wrestler",
+			division: "Varsity",
+			weightClass: "111",
+			position: 2
+		};
+		
+		const {rerender} = render(<TeamWrestlers wrestlers={ wrestlers } updateWrestlers={ updateWrestlers } addWrestler={ addWrestler } savingError={ "" } />);
+
+		const addButton = await screen.findByRole("button", { name: /^add wrestler$/i });
+		fireEvent.click(addButton);
+
+		const firstNameInput = await screen.findByLabelText(/^first name$/i),
+			lastNameInput = await screen.findByLabelText(/^last name$/i),
+			divisionInput = await screen.findByLabelText(/^wrestler division$/i),
+			weightClassInput = await screen.findByLabelText(/^wrestler weight class$/i),
+			saveButton = await screen.findByRole("button", { name: /^save$/i });
+
+		fireEvent.change(firstNameInput, { target: { value: newWrestler.firstName }});
+		fireEvent.change(lastNameInput, { target: { value: newWrestler.lastName }});
+		fireEvent.change(divisionInput, { target: { value: newWrestler.division }});
+		fireEvent.change(weightClassInput, { target: { value: newWrestler.weightClass }});
+
+		fireEvent.click(saveButton);
+
+		rerender(<TeamWrestlers wrestlers={ wrestlers.concat(newWrestler) } updateWrestlers={ updateWrestlers } addWrestler={ addWrestler } savingError={ "" } />)
+		
+		expect(await screen.findByTestId(newWrestler.id)).toBeInTheDocument();
+
+	});
+
+});
