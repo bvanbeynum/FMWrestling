33a2b226bc7173a0935abec43db94ef2ca0b7826
bvanbeynum <maildrop444@gmail.com>
2024-11-29 11:00:15 -0500

    Production build

--- client/portal/wrestlerdetails.jsx
+++ client/portal/wrestlerdetails.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState, useRef } from "react";
+import * as d3 from "d3";
 
 const WrestlerDetails = props => {
 
@@ -6,8 +7,11 @@ const WrestlerDetails = props => {
 	const [ errorMessage, setErrorMessage ] = useState("");
 	const [ wrestler, setWrestler ] = useState(null);
 	const [ selectedEvent, setSelectedEvent ] = useState(null);
+	const [ maxZoom, setMaxZoom ] = useState(400);
+	const [ currentZoom, setCurrentZoom ] = useState(100);
 
 	const panelRef = useRef();
+	const svgRef = useRef();
 
 	useEffect(() => {
 		if (panelRef.current) {
@@ -44,11 +48,95 @@ const WrestlerDetails = props => {
 								: event.matches?.some(match => match.winType && /^3rd place$/i.test(match.round) && match.isWinner) ? "3rd"
 								: event.matches?.some(match => match.winType && /^3rd place$/i.test(match.round) && !match.isWinner) ? "4th"
 								: "—"
+						})),
+						matches: data.wrestler.matches.map(match => ({
+							...match, 
+							date: new Date(match.date),
+							vs: match.winnerSqlId == data.wrestler.sqlId ? { sqlId: match.loserSqlId, name: match.loser, team: match.loserTeam } : { sqlId: match.winnerSqlId, name: match.winner, team: match.winnerTeam },
+							isWinner: match.winnerSqlId == data.wrestler.sqlId
 						}))
 					};
 
 					setWrestler(updatedWrestler);
 					props.updateWrestler(updatedWrestler);
+
+					const relationships = [...new Set(updatedWrestler.matches.map(match => match.vs.sqlId))]
+							.map(group => 
+								updatedWrestler.matches
+									.filter(match => match.vs.sqlId == group)
+									.sort((matchA, matchB) => +matchB.date - +matchA.date)
+									.map(match => ({
+										source: updatedWrestler.sqlId,
+										target: match.vs.sqlId,
+										vs: match.vs.name,
+										shade: (((Date.now() - match.date) / 1000 / 60 / 60 / 24 / 365) * 255) / 2
+									}))
+									.find(() => true)
+							)
+							.sort((opponentA, opponentB) => opponentA.age - opponentB.age),
+
+						nodes = relationships.map(relationship => ({ id: relationship.target, name: relationship.vs })).concat([{ id: updatedWrestler.sqlId, name: updatedWrestler.name }]);
+					
+					const svg = d3.select(svgRef.current);
+
+					const lines = svg.selectAll("line")
+						.data(relationships)
+						.enter()
+						.append("line")
+						.attr("stroke", d => `rgb(${ d.shade } ${ d.shade } ${ d.shade })`);
+					
+					const nodeSVG = svg.selectAll(".node")
+						.data(nodes)
+						.enter()
+						.append("g")
+						.attr("class", "node")
+						.each(function (parent) {
+							const node = d3.select(this);
+							
+							const textNode = document.createElementNS("http://www.w3.org/2000/svg", "text");
+							textNode.appendChild(document.createTextNode(parent.name));
+							console.log(textNode.getBBox())
+
+							// const bBox = node
+							// 	.append("text")
+							// 	.text(d => d.name)
+							// 	.node()
+							// 	.getBBox();
+							
+							node.append("rect")
+								.attr("width", 100) // Math.ceil(bBox.width) + 10
+								.attr("height", 30)
+								.attr("x", -5)
+								.attr("y", -20)
+								.attr("rx", "5");
+							
+							node.node().appendChild(textNode);
+						});
+					
+					// nodeSVG.append("rect")
+					// 	.attr("width", 100)
+					// 	.attr("height", 30)
+					// 	.attr("x", -5)
+					// 	.attr("y", -20)
+					// 	.attr("rx", "5");
+
+					// nodeSVG.append("text")
+					// 	.text(d => d.name);
+					
+					const simulation = d3.forceSimulation(nodes)
+						.force("link", d3.forceLink(relationships).id(d => d.id))
+						.force("collide", d3.forceCollide().radius(100))
+						.force("charge", d3.forceManyBody().strength(-300))
+						.force("center", d3.forceCenter(500, 500));
+					
+					simulation.on("tick", () => {
+						lines.attr("x1", d => d.source.x)
+							.attr("y1", d => d.source.y)
+							.attr("x2", d => d.target.x)
+							.attr("y2", d => d.target.y);
+						
+						nodeSVG.attr("transform", d => `translate(${d.x}, ${d.y})`);
+					});
 
 					setIsLoading(false);
 				})
@@ -201,6 +289,16 @@ const WrestlerDetails = props => {
 
 		</>
 		}
+
+		<div className="sectionHeading">Wrestling Network</div>
+
+		<div className="network">
+			<svg ref={ svgRef } viewBox="0 0 1000 1000" style={{ height: `${ currentZoom }%`, width: `${ currentZoom }%` }}></svg>
+		</div>
+
+		<div>
+			<input type="range" min="100" max={ maxZoom } value={ currentZoom } onChange={ event => setCurrentZoom(event.target.value) } step="1" />
+		</div>
 
 	</div>
 
